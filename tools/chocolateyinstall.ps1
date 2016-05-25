c42b55f1ac6644f829c015ec8de439401973f974
Valeri Vicneanschi <vicneanschi@gmail.com>
2016-05-25 15:30:40 -0400

    Fix metainformation

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -20,8 +20,8 @@ $packageArgs = @{
   #file         = $fileLocation
 
   #MSI
-  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
+  #silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0)
   #OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   #silentArgs   = '/S'           # NSIS
